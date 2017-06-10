5b4ba716d6795dff8bdb2496357e0864ae93c349
andy-shi88 <shi88.andy@gmail.com>
2017-06-10 16:34:20 +0700

    change approach, less loop, still timeout on several cases

--- minimum-loss/main.java
+++ minimum-loss/main.java
@@ -9,20 +9,33 @@ public class Solution {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
-        int[] arr = new int[n];
+        Node[] nodes = new Node[n];
         for(int i = 0; i < n; i++) {
-            arr[i]= in.nextInt();
+            int a = in.nextInt();
+            Node node = new Node(i, a);
+            nodes[i] = node;
         }
+        Arrays.sort(nodes);
         int min = Integer.MAX_VALUE;
-        for(int i = 0; i < n-1; i++) {
-            for (int j = i+1; j < n; j++) {
-                if(arr[i] > arr[j]) {
-                    if(arr[i] - arr[j] < min){
-                        min = arr[i]-arr[j];
-                    }
-                }
+        for(int i = 1; i< nodes.length; i++) {
+            if(nodes[i].index < nodes[i-1].index ) {
+                if(nodes[i].data - nodes[i-1].data < min)
+                    min = nodes[i].data - nodes[i-1].data;
             }
         }
         System.out.printf("%d", min);
+    }
+
+    static class Node implements Comparable<Node>{
+        public int index;
+        public int data;
+        Node(int index, int data){
+            this.index = index;
+            this.data = data;
+        }
+        public int compareTo(Node node) {
+            int data_b = node.data;
+            return this.data - data_b;
+        }
     }
 }
